ad18f3c200875904f4ef868141d55826d5c71c90
Kushalappa C A <kushalappa.ca@Incture.local>
2023-10-08 23:35:59 +0530

    testing push

--- src/main/java/com/prj/springsecurityjwt/SpringSecurityJwtApplication.java
+++ src/main/java/com/prj/springsecurityjwt/SpringSecurityJwtApplication.java
@@ -7,5 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringSecurityJwtApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityJwtApplication.class, args);
+		//testing push
 	}
 }
